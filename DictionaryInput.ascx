b0f0cb8eaf1f995bd3c63dc9babaedb548574928
Sid <sidsoft2014@outlook.com>
2017-08-26 14:45:52 +0000

    first commit

---
+++ DictionaryInput.ascx
@@ -0,0 +1,19 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DictionaryInput.ascx.cs" Inherits="CMSApp.CMSFormControls.DictionaryInput" ViewStateMode="Enabled" %>
+
+<asp:TextBox runat="server" ID="txbKey" />
+<asp:TextBox runat="server" ID="txbValue" />
+<asp:Button runat="server" ID="btnAdd" OnClick="btnAdd_Click" />
+<cms:CMSRepeater runat="server" ID="rptItems" OnItemDataBound="rptItems_ItemDataBound">
+    <HeaderTemplate>
+        <ul>
+    </HeaderTemplate>
+    <ItemTemplate>
+        <li>
+            <h3 runat="server" id="txtKey"></h3>
+            <p runat="server" id="txtValue"></p>
+        </li>
+    </ItemTemplate>
+    <FooterTemplate>
+        </ul>
+    </FooterTemplate>
+</cms:CMSRepeater>
---
+++ DictionaryInput.ascx.cs
@@ -0,0 +1,112 @@
+﻿using CMS.FormEngine.Web.UI;
+using CMS.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace CMSApp.CMSFormControls
+{
+    public partial class DictionaryInput : FormEngineUserControl
+    {
+        private Dictionary<string, string> _dic;
+        private string sKey;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            sKey = $"dicItems_{CurrentPageInfo.DocumentGUID}";
+        }
+
+        public override object Value
+        {
+            get
+            {
+                EnsureItems();
+                return ItemsToString();
+            }
+            set
+            {
+                var str = ValidationHelper.GetString(value, string.Empty);
+                EnsureItems();
+                if (!string.IsNullOrEmpty(str))
+                {
+                    SetValues(str);
+                }
+            }
+        }
+
+        protected void rptItems_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                try
+                {
+                    var data = (KeyValuePair<string, string>)e.Item.DataItem;
+                    var key = e.Item.FindControl("txtKey") as HtmlGenericControl;
+                    var val = e.Item.FindControl("txtValue") as HtmlGenericControl;
+
+                    key.InnerText = data.Key;
+                    val.InnerText = data.Value;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void EnsureItems()
+        {
+            if (Session[sKey] != null)
+            {
+                var json = Session[sKey] as string;
+                SetValues(json);
+            }
+            else
+            {
+                _dic = new Dictionary<string, string>();
+            }
+        }
+
+        private object ItemsToString()
+        {
+            return JsonConvert.SerializeObject(_dic);
+        }
+
+        private void SetValues(string str)
+        {
+            var json = str;
+            if (!string.IsNullOrEmpty(json))
+                _dic = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            Session[sKey] = ItemsToString();
+
+            rptItems.DataSource = _dic;
+            rptItems.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txbKey.Text.Length * txbValue.Text.Length == 0)
+                return;
+
+            if (!_dic.ContainsKey(txbKey.Text))
+                _dic.Add(txbKey.Text, txbValue.Text);
+            else
+                _dic[txbKey.Text] = txbValue.Text;
+
+            SetValues(null);
+        }
+
+        public override void Dispose()
+        {
+            Session[sKey] = null;
+            base.Dispose();
+        }
+    }
+}
