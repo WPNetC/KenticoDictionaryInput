0b34fa8efe5679ef9d5d8f3056feef8738716018
Sid <sidsoft2014@outlook.com>
2017-08-27 15:31:41 +0000

    Dynamic input

---
+++ DynamicInput.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DynamicInput.ascx.cs" Inherits="CMSApp.CMSFormControls.DynamicInput" %>
+
+<asp:TextBox runat="server" ID="txtElems" TextMode="Number" AutoPostBack="true" />
+<br />
+<asp:Panel runat="server" ID="pnlItems">
+</asp:Panel>
---
+++ DynamicInput.ascx.cs
@@ -0,0 +1,130 @@
+﻿using CMS.FormEngine.Web.UI;
+using CMS.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CMSApp.CMSFormControls
+{
+    public partial class DynamicInput : FormEngineUserControl
+    {
+        private const string _selector = "js-dynInput";
+        public int Elements
+        {
+            get
+            {
+                return ValidationHelper.GetInteger(GetValue("Elements"), 0);
+            }
+            set
+            {
+                SetValue("Elements", value);
+            }
+        }
+
+        public override object Value
+        {
+            get
+            {
+                return GetTextValues();
+            }
+            set
+            {
+                var strVal = Convert.ToString(value);
+                if (!string.IsNullOrEmpty(strVal) && strVal != "0")
+                {
+                    Populate(strVal);
+                }
+            }
+        }
+
+        private void Populate(string strVal)
+        {
+            if (pnlItems.Controls.Count == 1)
+            {
+                List<string> list = null;
+                try
+                {
+                    list = JsonConvert.DeserializeObject<List<string>>(strVal);
+                }
+                catch
+                {
+
+                }
+
+                if (list != null)
+                {
+                    Elements = list.Count;
+                    txtElems.Text = Elements.ToString();
+                    foreach (var item in list)
+                    {
+                        var txb = new TextBox()
+                        {
+                            Text = item,
+                            CssClass = $"form-control {_selector}"
+                        };
+                        pnlItems.Controls.Add(txb);
+                    }
+                }
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            int elems = -1;
+            Int32.TryParse(txtElems.Text, out elems);
+
+            if (elems != -1 && elems != Elements)
+            {
+                if (elems > Elements)
+                {
+                    int ii = Elements;
+                    for (; ii < elems; ii++)
+                    {
+                        var txt = new TextBox
+                        {
+                            CssClass = $"form-control {_selector}"
+                        };
+                        txt.Attributes["placeholder"] = "Enter text...";
+                        pnlItems.Controls.Add(txt);
+                    }
+                    Elements = elems;
+                    txtElems.Text = Elements.ToString();
+                }
+                else
+                {
+                    var tbs = pnlItems.Controls.OfType<TextBox>().ToList().Take(elems);
+                    pnlItems.Controls.Clear();
+                    foreach (var tb in tbs)
+                    {
+                        pnlItems.Controls.Add(tb);
+                    }
+                }
+            }
+        }
+
+        private string GetTextValues()
+        {
+            var tbs = pnlItems.Controls
+                .OfType<TextBox>()
+                .Where(p => !string.IsNullOrEmpty(p.Text) && p.CssClass.Contains(_selector))
+                .ToArray();
+
+            if (!tbs.Any())
+                return string.Empty;
+
+            var result = new List<string>();
+            foreach (var item in tbs)
+            {
+                result.Add(item.Text);
+            }
+
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}
